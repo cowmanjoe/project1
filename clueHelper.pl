0a137eb69300dd81814a5c999af86c10e0b53701
Cowan <ctcumming@gmail.com>
2016-10-22 15:28:26 -0700

    (see last commit).

--- clueHelper.pl
+++ clueHelper.pl
@@ -40,11 +40,20 @@ weapons(L) :- findall(X0, weapon(X0), L).
 %% rooms(L) is true if L is the list of all possible rooms
 rooms(L) :- findall(X0, room(X0), L).
 
+%% possible(P, W, R) is true if person P, weapon W and room R could make a 
+%% possible answer
 possible(P, W, R) :- person(P), weapon(W), room(R), \+ failed_guess(P, W, R). 
 
+%% does the same as possible but organized into a triple
 possible(a(P, W, R)) :- possible(P, W, R). 
 
+%% all_possibilities(L) is true when L is the list of all possible answers
 all_possibilities(L) :- findall(X0, possible(X0), L).
+
+
+%% total_possibilities(X) is true if X is the total number of possible combinations
+%% that the answer could be
+total_possibilities(X) :- all_possibilities(L), length(L, X). 
 
 %% possibilities(P, W, R, Chance) is true if the chance of P W and R being the answer
 %% is at least Chance
@@ -58,8 +67,7 @@ all_possibilities(L) :- findall(X0, possible(X0), L).
 %% chance of being the correct answer
 
 
-%% total_possibilities(X) is true if X is the total number of possible combinations
-%% that the answer could be
+
 
 %% failed_guess(P, W, R) is true if person P, weapon W, and room R have been guessed 
 %% and proven wrong i.e. one or more of the 3 is not the correct answer
